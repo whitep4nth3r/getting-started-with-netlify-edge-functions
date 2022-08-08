23c508b53c3730b5ac0b94a4770e2f5d96a5f9e7
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-08-08 13:52:11 +0100

    Add demo log line to hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,4 +1,6 @@
 export default async (request, context) => {
+  context.log("Calling hello.js edge function");
+
   return new Response("Hello, World from the edge!", {
     headers: { "content-type": "text/html" },
   });
