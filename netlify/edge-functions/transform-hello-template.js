be97b2a225af41b04d9f647664e0751187dc67fc
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-08-12 13:24:23 +0100

    Fix comments

--- netlify/edge-functions/transform-hello-template.js
+++ netlify/edge-functions/transform-hello-template.js
@@ -7,14 +7,14 @@ export default async (request, context) => {
   }
 
   // Get the page content that will be served next
-  // In this tutorial example, it will be the content from hello-html
+  // In this tutorial example, it will be the content from hello-template
   const response = await context.next();
   const page = await response.text();
 
   // Search for the placeholder
   const regex = /LOCATION_UNKNOWN/i;
 
-  // Get the location from the Context object
+  // Get the location from the context object
   const location = `${context.geo.city}, ${context.geo.country.name}`;
 
   // Replace the content with the current location
